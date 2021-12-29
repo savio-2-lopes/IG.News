365468540cc1cc76d5c049ac4eb79dd7768cff34
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-29 00:52:03 -0300

    Refactor app and fix bug

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from '../styles/home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className={styles.title}>
+        Hello World
+      </h1>
+    </div>
+  );
+}
