b5c48b0ff707c08c0498854d85b557153845e250
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-31 00:48:27 -0300

    :rocket: Init lesson Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 import styles from "./home.module.scss";
 
+// Client-side
+// Server-side
+// Static Site Generation
+
 interface HomeProps {
   product: {
     priceId: string;
