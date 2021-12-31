deee5306bc168fbe762f3cad8262285b11ed4ac9
savio-2-lopes <savioaugulopes@gmail.com>
2021-12-30 23:14:54 -0300

    :rocket: Refactor app and fix bug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import { SubscribeButton } from "../components/SubscribeButton";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,7 +8,24 @@ export default function Home() {
       <Head>
         <title>Home | Ig.News</title>
       </Head>
-      <h1>Hello World</h1>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👋🏽 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications
+            <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img draggable="false" src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
