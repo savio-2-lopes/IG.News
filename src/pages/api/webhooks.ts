9725fd935ae3e617b76fc74e653d8022f96adfd3
savio-2-lopes <savioaugulopes@gmail.com>
2022-02-06 10:29:25 -0300

    :rocket: Add app

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -54,7 +54,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             await saveSubscription(
               subscription.id,
               subscription.customer.toString(),
-              type === "customer.subscriptions.created"
+              false
             );
 
             break;
